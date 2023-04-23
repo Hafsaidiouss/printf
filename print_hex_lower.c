c913cfadec7bb53e0b2ffef4d813a75ed54da269
hafsaidiousse <hafsaidiousse@gmail.com>
2023-04-23 15:37:30 -0700

    hot fix

--- print_hex_lower.c
+++ print_hex_lower.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
 /**
  * print_hex_lower - print hex lower
  * @n: int
@@ -12,7 +13,7 @@ int print_hex_lower (unsigned int n)
 	if (n > 16)
 		c = c + print_hex_lower(n / 16);
 	if (n % 16 > 9)
-		_putchar('A' + n % 16 - 10);
+		_putchar('a' + n % 16 - 10);
 	else
 		_putchar(n % 16 + '0');
 	c++;
