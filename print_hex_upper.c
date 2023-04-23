1a33c2d500f83b043858e46b911f8cc624c9bedd
Salah <salah.elhssani@gmail.com>
2023-04-23 14:56:05 -0700

    Add print_hex_upper

---
+++ print_hex_upper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_hex_upper - print nnumber in base 16
+ * @n: number
+ *
+ * Return: number of character printed
+ */
+int print_hex_upper(unsigned int n)
+{
+	int c = 0;
+	int d;
+
+	if (n > 15)
+		c = c + print_hex_upper(n / 16);
+	d = n % 16;
+	if (d <= 10)
+		_putchar(d + '0');
+	else
+		_putchar(d + 'A');
+	c++;
+	return (c);
+}
