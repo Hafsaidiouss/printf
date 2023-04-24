1e44618aa7eff8cb8429d26ff7e7f6f3c11a8411
hafsaidiousse <hafsaidiousse@gmail.com>
2023-04-24 13:47:20 -0700

    task 7 + & space

--- print_number.c
+++ print_number.c
@@ -9,7 +9,15 @@ int print_number(int n, const  char **format, int fc)
 {
 	unsigned int i = n;
 	int c = 0;
+	int plus = 0;
+	int space = 0;
 
+	plus = flag(format, fc, '+');
+	space = flag(format, fc, ' ');
+	if (plus == 1 && n > 0)
+		c = c + _putchar('+');
+	if (space == 1 && plus == 0 && n > 0)
+		c = c + _putchar(' ');
 	if (n < 0)
 	{
 		_putchar('-');
