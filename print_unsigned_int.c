e5298014d8383202ee9c327e4aaa862dccdf6dc1
Salah <salah.elhssani@gmail.com>
2023-04-24 14:38:14 -0700

    Fix u_int case

--- print_unsigned_int.c
+++ print_unsigned_int.c
@@ -12,7 +12,7 @@ int print_unsigned_int(unsigned int n, const char **format, int fc)
 
 	if (i / 10)
 	{
-		c = c + print_number(i / 10, format, fc);
+		c = c + print_number(i / 10, format, 0);
 	}
 	_putchar(i % 10 + '0');
 	c++;
