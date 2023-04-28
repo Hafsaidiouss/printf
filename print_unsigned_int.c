894fd6aa4e84219f561fc5f5aa141270859b61f6
hafsaidiousse <hafsaidiousse@gmail.com>
2023-04-28 16:42:07 -0700

    fix indent

--- print_unsigned_int.c
+++ print_unsigned_int.c
@@ -11,14 +11,13 @@
  */
 int print_unsigned_int(va_list *ap, const char *format, int fc)
 {
-        int c = 0;
+	int c = 0;
 	int i;
 	int p;
-        int d;
-        unsigned long int pow;
-        int digit;
-        unsigned long int n;
-
+	int d;
+	unsigned long int pow;
+	int digit;
+	unsigned long int n;
 	int size = get_size(format, fc);
 	int width = get_width(ap, format, fc);
 	int precision = get_precision(ap, format, fc);
@@ -38,26 +37,23 @@ int print_unsigned_int(va_list *ap, const char *format, int fc)
 			n = (unsigned short) va_arg(*ap, unsigned int);
 			break;
 	}
-
 	if (n == 0 && precision == 0 && flag(format, fc, '.'))
-                return (0);
-        d = digit_count_unsigned(n, 10);
+		return (0);
+	d = digit_count_unsigned(n, 10);
 	p = (precision > d) ? precision - d : 0;
 	if (minus == 0)
 		c += print_padding(width, d + p, pc);
-	
 	c += print_padding(p, 0, '0');
 	for (i = d; i > 0; i--)
-        {
-                pow = _pow(10, i - 1);
-                digit = (n / pow);
-                c += _putchar(digit + '0');
-                n -= digit * pow;
-        }
-
+	{
+		pow = _pow(10, i - 1);
+		digit = (n / pow);
+		c += _putchar(digit + '0');
+		n -= digit * pow;
+	}
 	if (minus == 1)
-                c += print_padding(width, d + p, pc);
+		c += print_padding(width, d + p, pc);
 
-        return (c);
+	return (c);
 }
 
