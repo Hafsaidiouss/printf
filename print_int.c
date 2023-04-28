9a0118b124b5a0a6df8820f50d26672a29940bf7
hafsaidiousse <hafsaidiousse@gmail.com>
2023-04-28 16:46:58 -0700

    reduce lines

--- print_int.c
+++ print_int.c
@@ -15,23 +15,13 @@ int print_int(va_list *ap, const char *format, int fc)
 	int size = get_size(format, fc);
 	int precision = get_precision(ap, format, fc);
 	int width = get_width(ap, format, fc);
-
 	int plus = flag(format, fc, '+');
         int space = flag(format, fc, ' ') * (1 - plus);
         int minus = flag(format, fc, '-');
         int zero = flag(format, fc, '0') * (1 - minus);
         char pc = zero ? '0' : ' ';
-
-	int i;
-	int s;
-	int p;
-	int c = 0;
-	int sign = 1;
-	int d;
-	long int pow;
-	int digit;
-	long int n;
-
+	int i, s, p, c = 0, sign = 1, d, digit;
+	long int pow, n;
 
 	switch (size)
 	{
@@ -46,13 +36,11 @@ int print_int(va_list *ap, const char *format, int fc)
 			break;
 	}
 	if (n == 0 && precision == 0 && flag(format, fc, '.'))
-		return (0);
-	
+		return (0);	
 	d = digit_count_signed(n, 10);
 	p = (precision > d) ? precision - d : 0;
 	sign = ((n >= 0) ? 1 : -1);
 	s = (sign < 0) ? 1 : 0;
-
 	if (minus == 0 && zero == 0)
 		c += print_padding(width, d + s + p, pc);
 	if (sign == -1)
@@ -76,7 +64,5 @@ int print_int(va_list *ap, const char *format, int fc)
 	}
 	if (minus == 1)
 		c += print_padding(width, d + s + p, pc);
-
 	return (c);
-
 }
