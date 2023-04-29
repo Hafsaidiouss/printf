376e64bdbc8b1fb16e60203d3490833f05b8d3dc
hafsaidiousse <hafsaidiousse@gmail.com>
2023-04-28 17:07:55 -0700

    betty fixes

--- print_hex_upper.c
+++ print_hex_upper.c
@@ -11,19 +11,12 @@
  */
 int print_hex_upper(va_list *ap, const char *format, int fc)
 {
-        int c = 0;
-        int d;
-	int i;
-	int p;
-        unsigned long int pow;
-        char digit;
-        unsigned long int n;
-
+	int c = 0, d, i, p, digit;
+	unsigned long int pow, n;
 	int hash = flag(format, fc, '#');
 	int minus = flag(format, fc, '-');
 	int zero = flag(format, fc, '0') * (1 - minus);
 	char pc = zero ? '0' : ' ';
-
 	int size = get_size(format, fc);
 	int width = get_width(ap, format, fc);
 	int precision = get_precision(ap, format, fc);
@@ -40,36 +33,32 @@ int print_hex_upper(va_list *ap, const char *format, int fc)
 			n = (unsigned short) va_arg(*ap, unsigned int);
 			break;
 	}
-
 	if (n == 0 && precision == 0 && flag(format, fc, '.'))
-                return (0);
-        d = digit_count_unsigned(n, 16);
+		return (0);
+	d = digit_count_unsigned_int(n, 16);
 	p = (precision > d) ? precision - d : 0;
 	if (minus == 0)
 		c += print_padding(width, d + p + 2 * hash, pc);
-
 	if (hash == 1 && n != 0)
 		c += _print_str("0X");
 	p = (precision > d) ? precision - d : 0;
 	c += print_padding(p, 0, '0');
-        for (i = d; i - 1 > 0; i--)
-        {
-                pow = (long int) _pow(16, i - 1);
-                digit = n / pow;
-                n %= pow;
-                if (digit > 9)
-                        c += _putchar(digit - 10 + 'A');
-                else
-                        c += _putchar(digit + '0');
-        }
-
-        if (n > 9)
-                c += _putchar(n - 10 + 'A');
-        else
-                c += _putchar(n + '0');
-
+	for (i = d; i - 1 > 0; i--)
+	{
+		pow = _pow(16, i - 1);
+		digit = n / pow;
+		n %= pow;
+		if (digit > 9)
+			c += _putchar(digit - 10 + 'A');
+		else
+			c += _putchar(digit + '0');
+	}
+	if (n > 9)
+		c += _putchar(n - 10 + 'A');
+	else
+		c += _putchar(n + '0');
 	if (minus == 1)
 		c += print_padding(width, d + p + 2 * hash, pc);
-        return (c);
+	return (c);
 }
 
