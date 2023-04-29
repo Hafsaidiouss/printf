42adf296b3fc2f312cfec8fb816b785cdc674071
hafsaidiousse <hafsaidiousse@gmail.com>
2023-04-28 17:29:02 -0700

    fix print_binary.c

--- print_binary.c
+++ print_binary.c
@@ -11,26 +11,23 @@
  */
 int print_binary(va_list *ap, const char *format, int fc)
 {
-        int c = 0;
-        int d;
-        unsigned long int pow;
-        int digit;
-        unsigned int n = va_arg(*ap, unsigned int);
+	int c = 0;
+	int d;
+	unsigned long int pow;
+	int digit;
+	unsigned int n = va_arg(*ap, unsigned int);
 
 	(void) (format);
-        (void) (fc);
+	(void) (fc);
 
-
-        d = digit_count_unsigned(n, 2);
-	
+	d = digit_count_unsigned_int(n, 2);
 	for (; d > 0; d--)
-        {
-                pow = _pow(2, d - 1);
-                digit = (n / pow);
-                c += _putchar(digit + '0');
-                n -= digit * pow;
-        }
-
-        return (c);
+	{
+		pow = _pow(2, d - 1);
+		digit = (n / pow);
+		c += _putchar(digit + '0');
+		n -= digit * pow;
+	}
+	return (c);
 }
 
